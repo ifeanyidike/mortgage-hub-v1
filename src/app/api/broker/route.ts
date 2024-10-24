02ed77db10a229e8f09ec393c8a55ea223f878cc
Ifeanyi Dike <ifeanyi.dike@tryarbor.com>
2024-10-24 23:28:07 +0100

    remove invalid route

--- src/app/api/broker/route.ts
+++
@@ -1,8 +0,0 @@
-// src/app/api/users/route.ts
-
-import broker from "@/server/broker";
-import { type NextRequest } from "next/server";
-
-export const GET = (req: NextRequest) => {
-  return broker.getBrokers();
-};
